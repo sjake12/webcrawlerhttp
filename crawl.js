79e0d5b81207891bdeddb7c1b067a7ec947f3cbe
sjake12 <apostol.sjake12@gmail.com>
2024-09-08 10:36:18 +0800

    normalize url

---
+++ crawl.js
@@ -0,0 +1,12 @@
+function normalizeURL(urlString) {
+  const urlObj = new URL(urlString);
+  const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
+  if (hostPath.length > 0 && hostPath.slice(-1) === "/") {
+    return hostPath.slice(0, -1);
+  }
+  return hostPath;
+}
+
+module.exports = {
+  normalizeURL,
+};
